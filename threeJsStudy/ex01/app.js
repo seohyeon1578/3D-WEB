502e737d095c92373e72551941a6f2b5b9fa61a3
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2022-04-09 16:46:33 +0900

    Three.js 기본 구성

---
+++ threeJsStudy/ex01/app.js
@@ -0,0 +1,81 @@
+import * as THREE from '../../node_modules/three/build/three.module.js';
+class App {
+  constructor() {
+    const divContainer = document.getElementById("container");
+    this._divContainer = divContainer;  //다른 메서드에서 사용할 수 있도록
+
+    const randerar = new THREE.WebGLRenderer({ antialias: true });
+    randerar.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(randerar.domElement);
+    this._randerar = randerar;
+
+    const scene = new THREE.Scene();
+    this._scene = scene;
+
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      width / height,
+      0.1,
+      100
+    );
+    camera.position.z = 2;
+    this._camera = camera;
+  }
+
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this._scene.add(light);
+  }
+
+  _setupModel() {
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshPhongMaterial({color: 0x44a88});
+
+    const cube = new THREE.Mesh(geometry, material);
+
+    this._scene.add(cube);
+    this._cube = cube;
+  }
+
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._randerar.setSize(width, height);
+  }
+
+  render(time) {
+    this._randerar.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.001;
+    this._cube.rotation.x = time;
+    this._cube.rotation.y = time;
+  }
+}
+
+window.onload = function() {
+  new App();
+}
