27041b3277d609e2b221830ba324f9c535bb5fca
seohyeon1578 <84362569+seohyeon1578@users.noreply.github.com>
2022-04-12 18:14:51 +0900

    points,lines material

---
+++ threeJsStudy/ex06/material.js
@@ -0,0 +1,138 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+import { OrbitControls }from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+
+class App {
+  constructor() {
+    const divContainer = document.getElementById("container");
+    this._divContainer = divContainer;  //다른 메서드에서 사용할 수 있도록
+
+    const randerar = new THREE.WebGLRenderer({ antialias: true });
+    randerar.setPixelRatio(window.devicePixelRatio);
+    divContainer.appendChild(randerar.domElement);
+    this._randerar = randerar;
+
+    const scene = new THREE.Scene();
+    this._scene = scene;
+
+    this._setupCamera();
+    this._setupLight();
+    this._setupModel();
+    this._setupControls();
+
+    window.onresize = this.resize.bind(this);
+    this.resize();
+
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  _setupControls() {
+    new OrbitControls(this._camera, this._divContainer);
+  }
+
+  _setupCamera() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      width / height,
+      0.1,
+      100
+    );
+    camera.position.z = 7;
+    this._camera = camera;
+  }
+
+  _setupLight() {
+    const color = 0xffffff;
+    const intensity = 1;
+    const light = new THREE.DirectionalLight(color, intensity);
+    light.position.set(-1, 2, 4);
+    this._scene.add(light);
+  }
+
+  _setupModel() {
+    //points
+    // const vertices = [];
+    // for(let i = 0; i < 10000; i++) {
+    //   const x = THREE.Math.randFloatSpread(5);
+    //   const y = THREE.Math.randFloatSpread(5);
+    //   const z = THREE.Math.randFloatSpread(5);
+
+    //   vertices.push(x, y, z);
+    // }
+
+    // const geometry = new THREE.BufferGeometry();
+    // geometry.setAttribute(
+    //   "position",
+    //   new THREE.Float32BufferAttribute(vertices, 3)
+    // );
+
+    // const sprite = new THREE.TextureLoader().load(
+    //   "./sprite.png"
+    // );
+
+    // const material = new THREE.PointsMaterial({
+    //   map: sprite,
+    //   alphaTest: 0.5,     //원의 픽셀 값중 알파값이 alphaTest보다 클때만 렌더링
+    //   color: 0x00ffff,    //"yellow","#00ffff"
+    //   size: 0.1,
+    //   sizeAttenuation: true  //거리에 따른 포인트 크기조절을 할것인지
+    // });
+
+    // const points = new THREE.Points(geometry,material);
+    // this._scene.add(points);
+
+    //line
+    const vertices = [
+      -1,1,0,
+      1,1,0,
+      -1,-1,0,
+      1,-1,0,
+    ];
+
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute("position",
+      new THREE.Float32BufferAttribute(vertices, 3));
+  
+    // const material = new THREE.LineBasicMaterial({
+    //   color: "yellow"
+    // });
+
+    const material = new THREE.LineDashedMaterial({
+      color: "yellow",
+      dashSize: 0.2,
+      gapSize: 0.1,
+      scale: 1        
+    });
+
+    //const line = new THREE.Line(geometry,material);
+    //const line = new THREE.LineSegments(geometry,material);
+    const line = new THREE.LineLoop(geometry,material);
+    line.computeLineDistances();  //Dahsed
+    this._scene.add(line);
+  }
+
+  resize() {
+    const width = this._divContainer.clientWidth;
+    const height = this._divContainer.clientHeight;
+
+    this._camera.aspect = width / height;
+    this._camera.updateProjectionMatrix();
+
+    this._randerar.setSize(width, height);
+  }
+
+  render(time) {
+    this._randerar.render(this._scene, this._camera);
+    this.update(time);
+    requestAnimationFrame(this.render.bind(this));
+  }
+
+  update(time) {
+    time *= 0.001;
+  }
+}
+
+window.onload = function() {
+  new App();
+}
